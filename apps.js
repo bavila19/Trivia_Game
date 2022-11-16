1a880a03109d043e028cd80eb954e2935dac3e3a
bavila19 <brendaavila015@gmail.com>
2022-11-16 13:33:25 -0700

    styling updates

--- apps.js
+++ apps.js
@@ -1,24 +1,52 @@
-const startb = document.querySelector('.startbtn')
-const box_overlay = document.querySelector('.overlay')
-const correct_options = document.querySelector('.option-three')
-const box_two = document.querySelector('.boxtwo')
-
-
-console.log(correct_options)
-// console.log(box_overlay)
-startb.addEventListener("click",(event) =>{
+const correct_option1  = document.querySelector('.option-one')
+const box_one = document.querySelector('.box')
+correct_option1.addEventListener("click",(event) =>{
     event.preventDefault()
-    box_overlay.style.display="none"
+    correct_option1.style.backgroundColor="green"
+    box_one.style.display="block"
 });
 
+const incorrect_answer2 = document.querySelector('.option-two')
+incorrect_answer2.addEventListener("click",(event) =>{
+    event.preventDefault()
+    incorrect_answer2.style.backgroundColor="red"
+    box_one.style.display="block"
+});
+
+const incorrect_answer3= document.querySelector('.option-three')
+incorrect_answer2.addEventListener("click",(event) =>{
+    event.preventDefault()
+    incorrect_answer3.style.backgroundColor="red"
+    box_one.style.display="block"
+});
+// Card two
+const box_two = document.querySelector('.boxtwo')
+const correct_options = document.querySelector('.option-four')
 correct_options.addEventListener("click", (event) =>{
     event.preventDefault()
     // console.log ("it works")
     box_two.style.display="none"
+    
+});
+const incorrect_answer4= document.querySelector('.option-five')
+incorrect_answer4.addEventListener("click",(event) =>{
+    event.preventDefault()
+    incorrect_answer4.style.backgroundColor="red"
+    box_two.style.display="block"
+});
 
-})
+const incorrect_answer5= document.querySelector('.option-six')
+incorrect_answer5.addEventListener("click",(event) =>{
+    event.preventDefault()
+    incorrect_answer5.style.backgroundColor="red"
+    box_two.style.display="block"
+});
 
-// move/remove overy when clicked 
-// somehow move overlay to the center of the screant 
-//use class for option one & two 
+//start
+const startb = document.querySelector('.startbtn')
+const box_overlay = document.querySelector('.overlay')
+startb.addEventListener("click",(event) =>{
+    event.preventDefault()
+    box_overlay.style.display="none"})
+
 
